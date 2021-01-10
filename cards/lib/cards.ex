2ffc1cabb2df6c83a4cab7ab8c1e895d94d0e445
Pasit Tiwawongrut <maxkung55@gmail.com>
2021-01-10 19:52:57 +0700

    Added document for cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Proiveds methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Return list of cards in a deck
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -15,7 +22,35 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many card should be in hand.
+
+  ## Examples
+
+      iex > deck = Cards.create_deck
+      iex > {hand, deck} = Cards.deal(deck, 1)
+      iex > hand
+      ["Ace if Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "File not found"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
   end
 end
